3305359731b9334dcc6708e0cd64a4e1f2a70259
yanxiaolzay <1977735808@qq.com>
2021-06-04 10:10:36 +0800

    chore: update babel-loader configuration

--- webpack.config.js
+++ webpack.config.js
@@ -22,14 +22,7 @@ module.exports = (env) => {
       rules: [
         {
           test: /\.jsx?$/,
-          use: [
-            {
-              loader: "babel-loader",
-              options: {
-                presets: ["@babel/preset-env", "@babel/preset-react"]
-              }
-            }
-          ],
+          loader: "babel-loader",
         },
         {
           test: /\.css$/,
